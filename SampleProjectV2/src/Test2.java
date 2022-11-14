f3ee2902a34719078c3ca2a2609c224fefe81d20
mddanishazad3012 <danishazad3012@gmail.com>
2022-11-14 12:18:14 +0530

    Again  modified

--- SampleProjectV2/src/Test2.java
+++ SampleProjectV2/src/Test2.java
@@ -4,6 +4,7 @@ public class Test2 {
 	public static void main(String[] args) {
 		System.out.println("Nothing is impossible");
 		System.out.println("Everything is possible");
+		System.out.println("Danish");
 	}
 
 }
